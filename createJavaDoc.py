7d10e341fb54657e22930797d1f0a67ffa452475
Z0044J2 <aswanth.p@target.com>
2023-11-29 15:31:01 +0530

    added flow diagram

--- createJavaDoc.py
+++ createJavaDoc.py
@@ -5,7 +5,11 @@ import argparse
 
 # Replace with your OpenAI API key
 # api_key = "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"
-api_key = "temp"
+def get_opeai_key():
+      with open('key.txt', 'r') as file:
+        return file.read()
+      
+api_key = get_opeai_key()
 
 def getOpenAiResponse(prompt):
     try:
@@ -15,7 +19,7 @@ def getOpenAiResponse(prompt):
         max_tokens=150,  # Adjust this based on the desired response length
         api_key=api_key
         )
-        return response
+        return response.choices[0].text
     except Exception as err:
         print(traceback.format_exc())
         return "Got Exception from Completion API"
@@ -25,19 +29,24 @@ def processInputFile(java_file_path):
     with open(java_file_path, 'r') as file:
         java_code = file.read()
 
-    prompt = f"Generate Javadoc comments for the following Java class and each functions and return the commented java class for the file :\n{java_code}"
-    return prompt
+    # prompt = f"Generate Javadoc comments for the following Java class and each functions and return the commented java class for the file :\n{java_code}"
+    prompt = f"""Create Documentation file which contains folowwing sections for the given file
+                1. Identify functions
+                2. Identify dependencies
+                3. Initialization  
+                4. Flow diagram
+                Sample code:
+                \n{java_code}"""
 
-    # generated_comments = response.choices[0].text
-    # print(generated_comments)
+    generated_comments= getOpenAiResponse(prompt)
+
+    print(generated_comments)
 
 if __name__ == "__main__":
     try:
         parser = argparse.ArgumentParser()
         parser.add_argument('-p', '--path', type=str, help="Java File Path", required=True)
         java_file_path = vars(parser.parse_args())['path']
-        prompt=processInputFile(java_file_path)
-        print(prompt)
-        # getOpenAiResponse(prompt)
+        processInputFile(java_file_path)
     except Exception as err:
         print(traceback.format_exc())
